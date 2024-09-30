abc0059aabac41312cc48574a2608254f5069ae8
lmt2 <tamas.lang.miticzky@gmail.com>
2024-09-30 09:46:37 +0200

    Initial files for MakeCode project

---
+++ README.md
@@ -0,0 +1,24 @@
+
+> Open this page at [https://lmt2.github.io/micro_bit_labirintus/](https://lmt2.github.io/micro_bit_labirintus/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/lmt2/micro_bit_labirintus** and import
+
+## Edit this project
+
+To edit this repository in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/lmt2/micro_bit_labirintus** and click import
+
+#### Metadata (used for search, rendering)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,143 @@
+function moveForward () {
+    if (!(isOperating)) {
+        return
+    }
+    cuteBot.motors(speed, speed)
+    basic.clearScreen()
+    led.plot(0, 4)
+    led.plot(4, 4)
+}
+function turnLeft () {
+    if (!(isOperating)) {
+        return
+    }
+    cuteBot.motors(-1 * turnSpeed, turnSpeed)
+    basic.clearScreen()
+    led.plot(4, 2)
+    music.play(music.createSoundExpression(WaveShape.Square, 1, 4557, 219, 219, 100, SoundExpressionEffect.Vibrato, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
+    lastTurn = "left"
+}
+function calcRigthDist () {
+    rightDist = cuteBot.ultrasonic(cuteBot.SonarUnit.Centimeters)
+}
+function turnBack () {
+    if (!(isOperating)) {
+        return
+    }
+    cuteBot.stopcar()
+    if (randint(0, 1) == 1) {
+        music.play(music.tonePlayable(392, music.beat(BeatFraction.Sixteenth)), music.PlaybackMode.UntilDone)
+        cuteBot.motors(-1 * turnSpeed, turnSpeed)
+    } else {
+        music.play(music.tonePlayable(784, music.beat(BeatFraction.Sixteenth)), music.PlaybackMode.UntilDone)
+        cuteBot.motors(turnSpeed, -1 * turnSpeed)
+    }
+    basic.clearScreen()
+    led.plot(4, 0)
+    led.plot(0, 4)
+    control.waitMicros(600000)
+}
+input.onButtonPressed(Button.A, function () {
+    cuteBot.stopcar()
+    isOperating = false
+    cuteBot.closeheadlights()
+})
+function watchDog () {
+    heading = input.compassHeading()
+    if (Math.abs(heading - prevHeading) <= headingTreshold) {
+        haltCounter += 1
+    } else {
+        haltCounter = 0
+    }
+    prevHeading = heading
+    if (haltCounter > haltCounterMax) {
+        mustReverse = true
+        if (isOperating) {
+            music.play(music.tonePlayable(988, music.beat(BeatFraction.Sixteenth)), music.PlaybackMode.UntilDone)
+        }
+    } else {
+        mustReverse = false
+    }
+}
+function reverse () {
+    if (!(isOperating)) {
+        return
+    }
+    cuteBot.stopcar()
+    cuteBot.motors(-1 * turnSpeed, -1 * turnSpeed)
+    basic.clearScreen()
+    led.plot(0, 0)
+    led.plot(4, 0)
+    control.waitMicros(400000)
+}
+function init () {
+    music.setVolume(44)
+    isOperating = false
+    mustReverse = false
+    cuteBot.stopcar()
+    haltCounterMax = 5
+    headingTreshold = 20
+    speed = 18
+    turnSpeed = 25
+    leftDistMin = 8
+    rightDistMin = 8
+    lastTurn = "left"
+}
+function calcLeftDist () {
+    leftDist = sonar.ping(
+    DigitalPin.P2,
+    DigitalPin.P2,
+    PingUnit.Centimeters
+    )
+}
+function turnRight () {
+    if (!(isOperating)) {
+        return
+    }
+    cuteBot.motors(turnSpeed, -1 * turnSpeed)
+    basic.clearScreen()
+    led.plot(0, 2)
+    music.play(music.createSoundExpression(WaveShape.Sine, 5000, 57, 244, 244, 100, SoundExpressionEffect.Warble, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
+    lastTurn = "right"
+}
+input.onButtonPressed(Button.B, function () {
+    isOperating = true
+    cuteBot.colorLight(cuteBot.RGBLights.ALL, 0xffffff)
+})
+let leftDist = 0
+let rightDistMin = 0
+let leftDistMin = 0
+let mustReverse = false
+let haltCounterMax = 0
+let haltCounter = 0
+let headingTreshold = 0
+let prevHeading = 0
+let heading = 0
+let rightDist = 0
+let lastTurn = ""
+let turnSpeed = 0
+let speed = 0
+let isOperating = false
+init()
+basic.forever(function () {
+    calcRigthDist()
+    calcLeftDist()
+    watchDog()
+    if (mustReverse) {
+        reverse()
+        turnBack()
+    }
+    if (rightDist < rightDistMin) {
+        turnLeft()
+    }
+    if (leftDist < leftDistMin) {
+        turnRight()
+    }
+    if (leftDist < leftDistMin && rightDist < rightDistMin) {
+        reverse()
+        turnBack()
+    }
+    if (leftDist >= leftDistMin && rightDist >= rightDistMin) {
+        moveForward()
+    }
+})
